f603da6ebac1e737d7ca0fad1ee8e153cc1d54db
DESKTOP-7KRVTMP\86151 <1985367439@qq.com>
2021-04-28 21:20:31 +0800

    d

--- Alibaba-Cloud-Member/src/main/java/com/hxx/alibaba/test/Test.java
+++ Alibaba-Cloud-Member/src/main/java/com/hxx/alibaba/test/Test.java
@@ -8,10 +8,10 @@ public class Test {
 
     public static void main(String[] args) throws Exception{
         RestTemplate template=new RestTemplate();
-        for(int i=0;i<100000;i++){
+        for(int i=0;i<10;i++){
             String result = template.getForObject("http://localhost:8091/member/list", String.class);
-            System.out.println("result->"+result);
-            Thread.sleep(10);
+            System.out.println("result------------------------------------->"+result);
+            Thread.sleep(500);
         }
 
 
